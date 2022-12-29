e6e597d7bd400c638e1b20604a576a071f24940a
ClaudioFritzen <sdfritzen96@gmail.com>
2022-12-29 12:33:26 +0000

    criamos o banco e os usuarios

--- mychatapp/models.py
+++ mychatapp/models.py
@@ -6,10 +6,16 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     pic = models.ImageField(upload_to='img', blank=True, null=True)
+    friends = models.ManyToManyField('Friend', related_name='my_friends')
 
     def __str__(self):
         return self.name
 
 class Friend(models.Model):
-    pass
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.profile.name
+    
+
 
--- mychatapp/views.py
+++ mychatapp/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {}
+    user = request.user.profile
+    friends = user.friends.all()
+    context = {'user': user, 'friends': friends}
     return render(request, "mychatapp/index.html", context )
+
+
+def detail(request, pk):
+    context = {}
+    return render(request, 'mychatapp/detail.html', context)
