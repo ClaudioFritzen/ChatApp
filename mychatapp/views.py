22c1b0c0f3b9beb76ed18aa1b2597e7289013606
ClaudioFritzen <sdfritzen96@gmail.com>
2022-12-29 23:54:57 +0000

    colocamos o id na função amigo

--- mychatapp/views.py
+++ mychatapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Profile, Friend
 # Create your views here.
 def index(request):
     user = request.user.profile
@@ -9,5 +9,6 @@ def index(request):
 
 
 def detail(request, pk):
+    friend = Friend.objects.get(profile_id= pk)
     context = {}
     return render(request, 'mychatapp/detail.html', context)
